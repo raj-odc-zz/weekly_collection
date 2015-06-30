0d1ee8ec5433915ce64ddb152bb762d0b81814e0
selvaraj <ksraj_odc@yahoo.com>
2015-06-30 11:18:17 +0530

    Added the authentication with Devise gem and user signup as default

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'passenger'
 gem 'pg'
 gem 'slim-rails'
 gem 'therubyracer', :platform=>:ruby
+gem 'devise'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_21]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'visitors#index'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
